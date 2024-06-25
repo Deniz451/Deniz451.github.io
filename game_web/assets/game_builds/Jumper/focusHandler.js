08184ea63d1f4d8d4e5598eb5c34409ee6baa3c9
Deniz451 <denis.resitko@gmail.com>
2024-06-25 14:44:31 +0200

    update

--- game_web/assets/game_builds/Jumper/focusHandler.js
+++ game_web/assets/game_builds/Jumper/focusHandler.js
@@ -1,14 +1,15 @@
 document.addEventListener('visibilitychange', function() {
     if (document.hidden) {
         // Call the Unity function to pause the game
-        SendMessage('PauseMenuManager', 'PauseGame');
+        SendMessage('GameManager', 'PauseGame');
     } else {
         // Call the Unity function to resume the game
-        SendMessage('PauseMenuManager', 'ResumeGame');
+        SendMessage('GameManager', 'ResumeGame');
     }
 });
 
 function SendMessage(objectName, functionName) {
-    var unityInstance = unityInstance || UnityLoader.instantiate("unityContainer", "Build/YourGame.json");
-    unityInstance.SendMessage(objectName, functionName);
+    if (typeof unityInstance !== 'undefined' && unityInstance != null) {
+        unityInstance.SendMessage(objectName, functionName);
+    }
 }
